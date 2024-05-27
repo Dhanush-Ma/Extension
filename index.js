a57e281696358bc9240554d3f5851223f98844a0
Dhanush-Ma <iamdhanush02@gmail.com>
2024-05-27 15:53:07 +0530

    feat: added copy to clipboard and delete option

--- index.js
+++ index.js
@@ -1,71 +1,104 @@
-let myTitle = []
-let myURL = []
+let myTitle = [];
+let myURL = [];
 
-const titleEl = document.getElementById("title-el")
-const urlEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("save-input")
-const delBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("save-tab")
-const ulEl = document.getElementById("ul-el")
+const titleEl = document.getElementById("title-el");
+const urlEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("save-input");
+const delBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("save-tab");
+const ulEl = document.getElementById("ul-el");
+const copyEls = document.getElementsByClassName("copy");
 
 //to display the stored info while refreshing
-let TitlefromLocalStorage = JSON.parse(localStorage.getItem("Title"))
-let URLfromLocalStorage = JSON.parse(localStorage.getItem("Url"))
+let TitlefromLocalStorage = JSON.parse(localStorage.getItem("Title"));
+let URLfromLocalStorage = JSON.parse(localStorage.getItem("Url"));
 
-if(TitlefromLocalStorage && URLfromLocalStorage)
-{
-    myTitle = TitlefromLocalStorage
-    myURL = URLfromLocalStorage
-    renderURL(myTitle,myURL)
+function handleDelete(index) {
+  myTitle = myTitle.filter((_, i) => index !== i);
+  myURL = myURL.filter((_, i) => index !== i);
+
+  renderURL(myTitle, myURL);
+
+  localStorage.setItem("Title", JSON.stringify(myTitle));
+  localStorage.setItem("Url", JSON.stringify(myURL));
 }
 
-function renderURL(title,url){
-let listItems = ""
-for(let i=0; i<title.length; i++)
-{
-     listItems += `<li> <span>${title[i]} </span>: 
-                        <a href="${url[i]}" target="_blank" >${url[i]}</a>
-                    </li>`
-}
-ulEl.innerHTML = listItems
+if (TitlefromLocalStorage && URLfromLocalStorage) {
+  myTitle = TitlefromLocalStorage;
+  myURL = URLfromLocalStorage;
+  renderURL(myTitle, myURL);
 }
 
-//save input buuton
-inputBtn.addEventListener("click", function()
-{
-        if(titleEl.value && urlEl.value)
-        {
-            myTitle.push(titleEl.value)
-            myURL.push(urlEl.value)
-            renderURL(myTitle,myURL)
-            titleEl.value = ""
-            urlEl.value = ""
+function renderURL(title, url) {
+  let listItems = "";
+  for (let i = 0; i < title.length; i++) {
+    listItems += `
+     <li class="list-item" style="display: flex; justify-content: space-between"> 
+      <div>
+        <span>${title[i]} </span>: 
+        <a href="${url[i]}" target="_blank" >${url[i]}</a>
+      </div>
+      <div style="display: flex; justify-content: space-between; gap: 5px; cursor: pointer;">
+        <i class="fa-regular fa-copy" id="copy-${i}"></i>
+        <i class="fa fa-trash" id="delete-${i}" style="color: #ff0000;"></i>
+      </div>
+    </li>`;
+  }
 
-            localStorage.setItem("Title", JSON.stringify(myTitle));
-            localStorage.setItem("Url", JSON.stringify(myURL))
-        }
-        
-})
+  ulEl.innerHTML = listItems;
+
+  for (let i = 0; i < title.length; i++) {
+    document.getElementById(`copy-${i}`).addEventListener("click", () => {
+      navigator.clipboard.writeText(url[i]);
+    });
+
+    document.getElementById(`delete-${i}`).addEventListener("click", () => {
+      handleDelete(i);
+    });
+  }
+}
+
+// copyEls?.forEach((el) => {
+//   el.addEventListener("click", (e) => {
+//     console.log(e.target);
+//   });
+// });
+
+//save input button
+inputBtn.addEventListener("click", function () {
+  if (titleEl.value && urlEl.value) {
+    myTitle.push(titleEl.value);
+    myURL.push(urlEl.value);
+    renderURL(myTitle, myURL);
+    titleEl.value = "";
+    urlEl.value = "";
+
+    localStorage.setItem("Title", JSON.stringify(myTitle));
+    localStorage.setItem("Url", JSON.stringify(myURL));
+  }
+});
 
 //save tab button
-tabBtn.addEventListener("click",function(){
-       chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        if(titleEl.value){
-        myURL.push(tabs[0].url)
-        myTitle.push(titleEl.value)
-        localStorage.setItem("Title", JSON.stringify(myTitle));
-        localStorage.setItem("Url", JSON.stringify(myURL))
-        titleEl.value = ""
-        renderURL(myTitle,myURL)
-        }
-    })   
-    
-})
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (titleEl.value) {
+      myURL.push(tabs[0].url);
+      myTitle.push(titleEl.value);
+      localStorage.setItem("Title", JSON.stringify(myTitle));
+      localStorage.setItem("Url", JSON.stringify(myURL));
+      titleEl.value = "";
+      renderURL(myTitle, myURL);
+
+      localStorage.setItem("Title", JSON.stringify(myTitle));
+      localStorage.setItem("Url", JSON.stringify(myURL));
+    }
+  });
+});
 
 //delete buuton
-delBtn.addEventListener("dblclick", function(){
-    localStorage.clear()
-    myTitle = []
-    myURL = []
-    renderURL(myTitle,myURL)
-})
+delBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myTitle = [];
+  myURL = [];
+  renderURL(myTitle, myURL);
+});
